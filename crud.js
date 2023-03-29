99f841e0ce9bb67242a4eba8dfe9387e1a22d524
pzambao <pedrozambaoo@gmail.com>
2023-03-28 23:20:21 -0300

    basic crud: start

--- crud.js
+++ crud.js
@@ -6,20 +6,9 @@ server.use(express.json()); // Set express as a json before the request process
 
 const courses = ['First Course', 'Second Course', 'Third Course']
 
-// req = request data
-// res = request response to front-end
-
-// Query params = params used after the route
-//  Ex.: ?name=First+Course
-
-// Route params = params used in route
-//  Ex.: /courses/id
-
 // GET
 server.get('/courses/:index', (req, res) => {
   const { index } = req.params
 })
-
-
 
 server.listen(3000); // set 3000 as local server port
